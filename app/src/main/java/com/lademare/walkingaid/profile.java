914578f49d239aa9a8f675d600561c1b9a3cc64e
liekedm <48092045+liekedm@users.noreply.github.com>
2019-04-23 22:18:29 +0200

    App Layout

--- app/src/main/java/com/lademare/walkingaid/profile.java
+++ app/src/main/java/com/lademare/walkingaid/profile.java
@@ -8,18 +8,22 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class profile extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    EditText editName;
+public class profile extends AppCompatActivity{
+
     EditText editWeight;
     EditText editWeightbearing;
     EditText editMessege;
 
-    public static final String name = "name";
     public static final String weight = "weight";
     public static final String weightbearing = "weightbearing";
     public static final String messege = "messege";
@@ -32,10 +36,16 @@ public class profile extends AppCompatActivity {
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
 
-        editName = findViewById(R.id.editName);
         editWeight = findViewById(R.id.editWeight);
         editWeightbearing = findViewById(R.id.editWeightbearing);
         editMessege = findViewById(R.id.editMessege);
+        Spinner spinner = findViewById(R.id.editLimb);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.limb_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        SharedPreferences sp = getSharedPreferences("sharedprefs", Activity.MODE_PRIVATE);
+        spinner.setSelection(sp.getInt("position",0));
+        spinner.setAdapter(adapter);
 
         menu();
         getdata();
@@ -64,7 +74,6 @@ public class profile extends AppCompatActivity {
 
     protected void getdata() {
         SharedPreferences sp = getSharedPreferences("sharedprefs", Activity.MODE_PRIVATE);
-        editName.setText(sp.getString(name, "Firstname Lastname"));
         editWeight.setText(String.valueOf(sp.getInt(weight, 80)));
         editWeightbearing.setText(String.valueOf(sp.getInt(weightbearing, 80)));
         editMessege.setText(sp.getString(messege, " "));
@@ -73,16 +82,17 @@ public class profile extends AppCompatActivity {
     protected void editdata() {
         SharedPreferences sp = getSharedPreferences("sharedprefs", Activity.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
-        String name = editName.getText().toString();
+        final Spinner spinner = findViewById(R.id.editLimb);
+        editor.putInt("position", spinner.getSelectedItemPosition());
+        String spinnerlimb = spinner.getSelectedItem().toString();
         int weight = Integer.parseInt(editWeight.getText().toString());
         int weightbearing = Integer.parseInt(editWeightbearing.getText().toString());
         String messege = editMessege.getText().toString();
-        editor.putString("name", name);
+        editor.putString("limb", spinnerlimb);
         editor.putInt("weight", weight);
         editor.putInt("weightbearing", weightbearing);
         editor.putString("messege", messege);
         editor.apply();
     }
-
 
 }
